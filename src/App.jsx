546f886beda60d834e1909f5f781104c8e486efe
Débora Linhares <deboralinharespinheiro@gmail.com>
2023-07-17 20:29:13 -0300

    projeto React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <div  className="App">
+      <h2> FILMES</h2>
+      </div>
+  )
+}
+export default App
+
+  //const urlAPI = "https://api.themoviedb.org/3/movie/top_rated?api_key=20dee6f042839bec76c473cdaf7b5f9e";
+  //fetch(urlAPI)
